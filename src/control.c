12edb9b9d41314d66709871330c3cb5b0b2b4ce7
Stefan Wallentowitz <stefan@wallentowitz.de>
2016-05-13 13:25:16 +0200

    Cleanup debug artifacts

--- src/control.c
+++ src/control.c
@@ -99,8 +99,6 @@ int osd_module_stall(struct osd_context *ctx, uint16_t id) {
 
 OSD_EXPORT
 int osd_module_unstall(struct osd_context *ctx, uint16_t id) {
-    printf("UNSTALL\n");
-    printf("to: %d\n", osd_modid2addr(ctx, id));
     osd_reg_write16(ctx, osd_modid2addr(ctx, id), OSD_REG_CS, OSD_CS_UNSTALL);
     return 0;
 }
