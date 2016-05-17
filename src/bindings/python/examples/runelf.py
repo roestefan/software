86c6460c525991e2eff739739f2548a11c2499a6
Stefan Wallentowitz <stefan@wallentowitz.de>
2016-05-17 17:13:33 +0200

    Reactivate MAM in python

--- src/bindings/python/examples/runelf.py
+++ src/bindings/python/examples/runelf.py
@@ -17,8 +17,8 @@ for m in osd.get_modules("STM"):
 for m in osd.get_modules("CTM"):
     m.log("ctm{:03x}.log".format(m.get_id()), elffile)
 
-#for m in osd.get_modules("MAM"):
-#    m.loadelf(elffile)
+for m in osd.get_modules("MAM"):
+    m.loadelf(elffile)
 
 osd.start()
 osd.wait(10)
